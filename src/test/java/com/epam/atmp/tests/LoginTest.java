169e39573f4b281926e2352e0ed82b890807dcb6
AlexanderUrbanovich <aliaksandr_urbanovich@epam.com>
2021-08-16 16:59:18 +0300

    Added reporter to reportportal

--- src/test/java/com/epam/atmp/tests/LoginTest.java
+++ src/test/java/com/epam/atmp/tests/LoginTest.java
@@ -1,7 +1,13 @@
+package com.epam.atmp.tests;
+
 import com.epam.atmp.model.User;
 import com.epam.atmp.service.UserCreator;
+import com.epam.atmp.tests.CommonConditions;
+import com.epam.reportportal.testng.ReportPortalTestNGListener;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners({ReportPortalTestNGListener.class})
 public class LoginTest extends CommonConditions {
 
     @Test
