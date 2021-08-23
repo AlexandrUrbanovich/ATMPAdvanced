55073b52c108d92d304fc0e1f1ded7666b00626a
AlexanderUrbanovich <aliaksandr_urbanovich@epam.com>
2021-08-23 17:46:01 +0300

    Test runner has been added

---
+++ src/test/java/com/epam/atmp/ProgramRunner.java
@@ -0,0 +1,17 @@
+package com.epam.atmp;
+
+import com.epam.atmp.tests.LoginTest;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+
+public class ProgramRunner {
+
+    public static void main(String args[]) {
+        TestListenerAdapter tla = new TestListenerAdapter();
+        TestNG testng = new TestNG();
+        testng.setTestClasses(new Class[] {LoginTest.class});
+        testng.addListener(tla);
+        testng.run();
+
+    }
+}
